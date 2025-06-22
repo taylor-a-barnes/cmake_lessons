ee4cfae91df97d07485a85630ac4569b45632341
Taylor Barnes <taylor.a.barnes@gmail.com>
2025-06-21 21:04:08 -0400

    Initial files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <yaml-cpp/yaml.h>
 
 int main() {
-  std::cout << "Hello World!" << '\n';
+  YAML::Node books = YAML::LoadFile("data/books.yaml");
+
+
+  for (const auto& pair : books["books"]) {
+    const auto& book = pair.second;
+
+    std::cout << book["name"] << "\n";
+    std::cout << "    Pages: " << book["pages"].as<std::string>() << "\n";
+    std::cout << "\n";
+  }
+
   return 0;
 }
