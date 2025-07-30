30454bbe9d860146bd22c88b31994b931f741e00
Taylor Barnes <taylor.a.barnes@gmail.com>
2025-07-30 12:26:39 -0400

    Remove yaml-cpp dependency

--- exe/src/main.cpp
+++ exe/src/main.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
-#include <yaml-cpp/yaml.h>
 #include <cmpwrapper.h>
 
 int main() {
-  YAML::Node books = YAML::LoadFile("data/books.yaml");
-
-  for (const auto& pair : books["books"]) {
-    const auto& book = pair.second;
-
-    std::cout << book["name"] << "\n";
-    std::cout << "    Pages: " << book["pages"].as<std::string>() << "\n";
-    std::cout << "\n";
-  }
 
   // Compress the file
   bool success = compress("data/books.yaml", "books.br");
@@ -19,7 +9,6 @@ int main() {
     std::cerr << "Compression failed.\n";
     return 1;
   }
-
 
   return 0;
 }
